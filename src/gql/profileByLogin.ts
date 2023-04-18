69f44b521ea63190869aab7c3a9c37f6875cbaef
Vitalii Maslianok <maslianok@gmail.com>
2023-04-19 00:22:51 +0200

    update query

--- src/gql/profileByLogin.ts
+++ src/gql/profileByLogin.ts
@@ -4,8 +4,30 @@ export default gql`
   query profileByLogin($login: String!) {
     profile(where: { login: { _eq: $login } }) {
       id
+      login
       name
       location
+      ownedRepoStar
+      collaboratedRepoStar
+      orgRepoStar
+      countryRanks {
+        profileId
+        countryInfo {
+          name
+        }
+        positionOwnedRepoStar
+        positionFollowersCount
+        positionContributionsCount
+        positionIssueContributions
+        positionCommitContributions
+        positionPullRequestContributions
+        positionPullRequestReviewContributions
+        positionRepositoriesWithContributedIssues
+        positionRepositoriesWithContributedCommits
+        positionRepositoriesWithContributedPullRequests
+        positionRepositoriesWithContributedPullRequestReviews
+        updatedAt
+      }
     }
   }
 `;
